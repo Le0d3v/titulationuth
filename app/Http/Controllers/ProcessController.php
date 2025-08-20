8ff2e4729eab478ef7300e153f11f93e5c3faf20
Le0d3v <harrywilsonth01@gmail.com>
2025-08-19 18:03:14 -0600

    Inicios de Frontend para sección de procesos (Admin)

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Process;
+use App\Models\DataSchool;
 use Illuminate\Http\Request;
 
 class ProcessController extends Controller
 {
     public function index()  {
-        $processes = Process::all();
+        $students6 = DataSchool::where("semester", "6")->get();
+        $students11 = DataSchool::where("semester", "11")->get(); 
+
         return view("admin.process.index", [
-            "processes" => $processes
+            "students11" => $students11,
+            "students6" => $students6,
         ]);
     }
 }
--- resources/views/admin/process/index.blade.php
+++ resources/views/admin/process/index.blade.php
@@ -17,6 +17,41 @@
                         <h2 class="text-center font-bold text-green-400 text-2xl">
                             TSU
                         </h2>
+                        <p class="text-sm text-gray-400">
+                            Estudiantes:
+                        </p>
+                        <div class="mt-5">
+                            @foreach ($students6 as $student)
+                                <div class="my-3 flex justify-between p-5 border-2 rounded border-green-500">
+                                    <div>
+                                        <h2 class="text-green-400 font-bold text-xl">
+                                            Información General
+                                        </h2>
+                                        <div class="mt-5">
+                                            <p class="text-gray-400">
+                                                Nombre:
+                                                <span class="text-black font-bold">
+                                                    {{$student->user->name}}
+                                                </spanc>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h2 class="text-green-400 font-bold text-xl mb-5">
+                                            Acciones
+                                        </h2>
+                                        <div class="mt-5">
+                                            <a 
+                                                href="#"
+                                                class="mt-5 p-2 bg-blue-500 text-white font-bold hover:bg-blue-700 rounded"
+                                            >
+                                                Ver Proceso
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                     <div class="w-full flex justify-center items-center p-5 mt-3">
                         <div class="w-full p-3 shadow-md">
